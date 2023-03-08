071057d2453a16b89abe2cde5248c4c40052800e
Bjoern Hoehrmann <bjoern@hoehrmann.de>
2023-03-08 21:50:16 +0100

    Why would conan 2.0 break so simple things without a migration guide discussing these changes? :(

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -4,7 +4,6 @@
 # conan profile new default --detect
 
 conan profile detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
 build_dir="$(realpath "$(dirname "$BASH_SOURCE")")"/../build
 mkdir -p "$build_dir"
-cd "$build_dir" && conan install ../conanfile.txt
+cd "$build_dir" && conan install -s compiler.libcxx=libstdc++11 ../conanfile.txt
