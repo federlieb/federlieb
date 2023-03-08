3ab3018599161764eee69d6629d8b8c08098f081
Bjoern Hoehrmann <bjoern@hoehrmann.de>
2023-03-08 21:00:12 +0100

    conan 2.x compatibility

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-conan profile new default --detect
+
+# conan 1.x
+# conan profile new default --detect
+
+conan profile detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
 build_dir="$(realpath "$(dirname "$BASH_SOURCE")")"/../build
 mkdir -p "$build_dir"
