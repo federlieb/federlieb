68f278e2fb471b864e0ede0edd4d3d653bfbfd71
Bjoern Hoehrmann <bjoern@hoehrmann.de>
2023-03-09 18:00:18 +0100

    .

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+python3 -m venv .venv
+. .venv/bin/activate
+pip3 install conan rich pytest pysqlite3-binary networkx
 conan profile detect
 build_dir="$(realpath "$(dirname "$BASH_SOURCE")")"/../build
 mkdir -p "$build_dir"
